f4ce783b9274753661803e44e56b4ebc9c0ce77a
johan101429 <jona1014.mr@gmail.com>
2024-06-13 09:21:03 -0500

    se crea boton para regresar a la pagina principal del instrumento

--- src/components/Layouts/Paint/Paint.jsx
+++ src/components/Layouts/Paint/Paint.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Paint.css'
 import { RandomImages } from '../../RandomImages/RandomImages'
 
+
+//se crea boton para regresar a la pagina principal del instrumento 
+
 export const Paint = () => {
 
   const randomNumber = (min,max) => Math.random() * (max - min) + min
@@ -16,6 +19,8 @@ export const Paint = () => {
     <div className='paint'>
       <img src="" alt="" />
       <button className='btnPictures' onClick={handleImages}>Imagen aleatoria</button>
+      <button type="button"><a href='/#'>Ingresar</a> </button> 
+
     </div>
   )
 }
