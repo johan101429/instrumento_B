6ff14d2ff36661fbec2e7c751b52352377f7ede8
johan101429 <jona1014.mr@gmail.com>
2024-06-13 09:13:57 -0500

    // se crea boton que lleva al componente Paint

--- src/components/Layouts/Welcome/Welcome.jsx
+++ src/components/Layouts/Welcome/Welcome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Paint} from '../Paint/Paint'
 
 export const Welcome = ({ ficha }) => {
   return (
     <>
       <h1>instrumento de evaluación</h1>
       <h2>Ficha: {ficha} </h2>
-      <a href='#'>Ingresar</a>
+      // se crea boton que lleva al componente Paint 
+      <button type="button"><a href='/Paint'>Ingresar</a> </button>
+      
     </>
   )
 }
