21e5f0c3d381f533bcc6716d4459d2826a19ff32
johan101429 <jona1014.mr@gmail.com>
2024-06-13 09:11:24 -0500

    se utiliza la funcion de route y routes para la cracion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import { Paint } from './components/Layouts/Paint/Paint'
 import { Welcome } from './components/Layouts/Welcome/Welcome'
 
+
 function App() {
-  
+  // se utiliza la funcion router para la creacdion de las routes
   return (
     <>
-      <Paint />      
+    <Routes>
+      <Route path='/' element={<Welcome />} />
+      <Route path='/paint' element={<Paint />} />
+    </Routes>
+    
+
+     
+     
+         
     </>
   )
 }
