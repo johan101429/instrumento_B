7a0a890e31dab6ad84df925fdd913d0868475acc
johan101429 <jona1014.mr@gmail.com>
2024-06-13 09:12:26 -0500

    se utiliza el elemento BrowserRouter par la renderizar el elemento

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // se utiliza el elemento BrowserRouter par la renderizar el elemento
+  <BrowserRouter>
+
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+
 )
